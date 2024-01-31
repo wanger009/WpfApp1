3cf775fa7cd8f5fd74362b81169ea007ef31effb
Urbán Balázs <ubalazs29@gmail.com>
2024-01-31 13:44:23 +0100

    kezdő felület

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -15,14 +15,29 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MinimumButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MaximumButton_Click(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
